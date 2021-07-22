a776dfd69a5a7c430e188439bd896188bc6d42a6
tico <lours2021@gmail.com>
2021-07-22 16:30:50 +0900

    Update: delete token & add command

--- src/main/java/BetBot.java
+++ src/main/java/BetBot.java
@@ -3,8 +3,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -14,44 +13,45 @@ import javax.security.auth.login.LoginException;
 
 public class BetBot extends ListenerAdapter {
 
-    private final static String token = "ODY2OTkzMjM1Njk5NTY0NTk0.YPaoXw.BZUGmpNg8lkxirBBO0sFx_0GRfY";
-
-    public static void main(String[] args)
-            throws LoginException
-    {
-
+    public static void main(String[] args) throws LoginException {
         if (args.length < 1) {
             System.out.println("You have to provide a token as first argument!");
             System.exit(1);
         }
         // args[0] should be the token
-        // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
-        // All other events will be disabled.
-        JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+        // We don't need any intents for this bot. Slash commands work without any intents!
+        JDA jda = JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new BetBot())
-                .setActivity(Activity.playing("Type !ping"))
-                .setActivity(Activity.playing("Type /ping"))
+                .setActivity(Activity.competing("Bet Bot!"))
                 .build();
+
+        jda.upsertCommand("ping", "Calculate ping of the bot").queue(); // This can take up to 1 hour to show up in the client
     }
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event)
-    {
+    public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
-        if (msg.getContentRaw().equals("!ping"))
-        {
-            MessageChannel channel = event.getChannel();
+        MessageChannel channel = event.getChannel();
+
+        if (msg.getContentRaw().equals("~~ping")) {
             long time = System.currentTimeMillis();
             channel.sendMessage("Pong!") /* => RestAction<Message> */
                     .queue(response -> {
                         response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
                     });
         }
+
+        if (msg.getContentRaw().equals("~~hello")) {
+            User user = event.getAuthor();
+            channel.sendMessage("Pong!") /* => RestAction<Message> */
+                    .queue(response -> {
+                        response.editMessageFormat("Hello! " + user.getAsMention()).queue();
+                    });
+        }
     }
 
     @Override
-    public void onSlashCommand(SlashCommandEvent event)
-    {
+    public void onSlashCommand(SlashCommandEvent event) {
         if (!event.getName().equals("ping")) return; // make sure we handle the right command
         long time = System.currentTimeMillis();
         event.reply("Pong!").setEphemeral(true) // reply or acknowledge
