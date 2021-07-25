b9b1851ff730338fde49728607e34451a99ebba6
tico <lours2021@gmail.com>
2021-07-25 19:17:42 +0900

    Add: user list command

--- src/main/java/BetBot.java
+++ src/main/java/BetBot.java
@@ -1,17 +1,19 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class BetBot extends ListenerAdapter {
+
+    private final static Set<User> users = new HashSet<>();
 
     public static void main(String[] args) throws LoginException {
         if (args.length < 1) {
@@ -28,25 +30,90 @@ public class BetBot extends ListenerAdapter {
         jda.upsertCommand("ping", "Calculate ping of the bot").queue(); // This can take up to 1 hour to show up in the client
     }
 
+    private static void showUsers(MessageChannel channel) {
+        int i = 1;
+        channel.sendMessage("User List").queue();
+        for (User u : users) {
+            channel.sendMessage(i++ + ". " + u.getAsMention()).queue();
+        }
+    }
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
         MessageChannel channel = event.getChannel();
+        User user = event.getAuthor();
 
-        if (msg.getContentRaw().equals("~~ping")) {
-            long time = System.currentTimeMillis();
-            channel.sendMessage("Pong!") /* => RestAction<Message> */
-                    .queue(response -> {
-                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                    });
+        if (user.isBot()) {
+            return;
         }
 
-        if (msg.getContentRaw().equals("~~hello")) {
-            User user = event.getAuthor();
-            channel.sendMessage("Pong!") /* => RestAction<Message> */
-                    .queue(response -> {
-                        response.editMessageFormat("Hello! " + user.getAsMention()).queue();
-                    });
+        if (msg.getContentRaw().startsWith("~~")) {
+            String[] commandArgs = msg.getContentRaw().substring(2).split(" ");
+
+            if (commandArgs[0].equalsIgnoreCase("help")) {
+                channel.sendMessage("Help!").queue();
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("ping")) {
+                long time = System.currentTimeMillis();
+                channel.sendMessage("Pong!") /* => RestAction<Message> */
+                        .queue(response -> {
+                            response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
+                        });
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("hello")) {
+                channel.sendMessage("Hello! " + user.getAsMention()).queue();
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("who")) {
+                showUsers(channel);
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("set") && commandArgs.length == 1) {
+                users.add(user);
+                channel.sendMessage("Set " + user.getAsMention()).queue();
+                showUsers(channel);
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("set") && commandArgs.length > 1) {
+                for (User u : event.getJDA().getUsers()) {
+                    channel.sendMessage(":: " + u.getAsMention()).queue();
+                }
+
+                try {
+                    User target = event.getJDA().getUserByTag(commandArgs[1]);
+                    users.add(target);
+                    channel.sendMessage("Set " + target.getAsMention()).queue();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    channel.sendMessage("No User").queue();
+                }
+                showUsers(channel);
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("reset") && commandArgs.length == 1) {
+                users.remove(user);
+                channel.sendMessage("reset " + user.getAsMention()).queue();
+                showUsers(channel);
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("reset") && commandArgs.length > 1) {
+                if (commandArgs[1].equalsIgnoreCase("all")) {
+                    users.clear();
+                } else {
+                    channel.sendMessage("reset " + user.getAsMention()).queue();
+                    showUsers(channel);
+                }
+            }
+
+            if (commandArgs[0].equalsIgnoreCase("dice")) {
+                int i = 1;
+                for (User u : users) {
+                    channel.sendMessage(i++ + ". " + u.getAsMention() + ": " + Math.round(Math.random() * 100)).queue();
+                }
+            }
         }
     }
 
