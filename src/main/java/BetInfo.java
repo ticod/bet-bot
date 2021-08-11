2d0e1d891b5fcec3ced72a04e929d83c082d08ef
tico <lours2021@gmail.com>
2021-08-12 03:32:00 +0900

    Update: BetStatus & lombok annotation

--- src/main/java/BetInfo.java
+++ src/main/java/BetInfo.java
@@ -1,15 +1,18 @@
+import lombok.Getter;
+import lombok.Setter;
 import net.dv8tion.jda.api.entities.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter @Setter
 public class BetInfo {
     private String title;
     private User user;
     private Integer totalPoint;
     private Map<User, Integer> agree;
     private Map<User, Integer> disagree;
-    private BetStatus betStatus;
+    private BetStatus betStatus = BetStatus.START;
 
     public BetInfo() {
         this.title = "";
@@ -17,54 +20,6 @@ public class BetInfo {
         this.totalPoint = 0;
         this.agree = new HashMap<>();
         this.disagree = new HashMap<>();
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getTotalPoint() {
-        return totalPoint;
-    }
-
-    public void setTotalPoint(Integer totalPoint) {
-        this.totalPoint = totalPoint;
-    }
-
-    public Map<User, Integer> getAgree() {
-        return agree;
-    }
-
-    public void setAgree(Map<User, Integer> agree) {
-        this.agree = agree;
-    }
-
-    public Map<User, Integer> getDisagree() {
-        return disagree;
-    }
-
-    public void setDisagree(Map<User, Integer> disagree) {
-        this.disagree = disagree;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public BetStatus getBetStatus() {
-        return betStatus;
-    }
-
-    public void setBetStatus(BetStatus betStatus) {
-        this.betStatus = betStatus;
     }
 
     public void clear() {
