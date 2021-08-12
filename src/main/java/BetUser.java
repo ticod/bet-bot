4d1603638696b47fcb1e49214a6fcfdb0fae6249
tico <lours2021@gmail.com>
2021-08-12 21:45:58 +0900

    Update: Equals lombok 추가

--- src/main/java/BetUser.java
+++ src/main/java/BetUser.java
@@ -1,10 +1,12 @@
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import net.dv8tion.jda.api.entities.User;
 
 @Getter @Setter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class BetUser {
     private User user;
     private Integer point;
